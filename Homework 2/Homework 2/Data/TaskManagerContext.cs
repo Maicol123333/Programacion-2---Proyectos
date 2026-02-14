ac78ac2d8c092382616dfd6524881aa1a9869274
Maicol123333 <suarezquezadamaicoljhoan@gmail.com>
2026-02-13 21:24:22 -0400

    commit of my Web API

---
+++ Homework 2/Homework 2/Data/TaskManagerContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskManagerApi
+{
+    public class TaskManagerContext : DbContext
+    {
+        public TaskManagerContext(DbContextOptions<TaskManagerContext> options) : base(options) { }
+        
+        public DbSet <TaskItem> TaskItems { get; set; }
+
+    }
+}
---
+++ Homework 2/Homework 2/Models/Entity/TaskItem.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskManagerApi
+{
+    public class TaskItem
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public bool IsCompleted { get; set; } = false;
+        public DateOnly CreateDate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+    }
+}
